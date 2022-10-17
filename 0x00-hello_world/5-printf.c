49de62edfe9354313ae9130f9d5b1f238846bcc7
Viklick29 <vedeh508@gmail.com>
2022-10-17 01:34:58 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/*
+ *main-entry point for the program.
+ *
+ *return:0 if no errors,return non-zero, if no errors
+ */
+int main (void)
+{
+  printf("with proper grammer,but the outcome is a piece of art,\n");
+  return(0);
+}
