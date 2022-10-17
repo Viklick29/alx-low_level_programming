f5723c49109a1a3d39d9df9b7bd9cc95826877c6
Viklick29 <vedeh508@gmail.com>
2022-10-17 11:22:00 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
- *main serves as the entry point for the program
+ *main- serves as the entry point for the program
+ *
  *
  *Return: by default returns zero if no errors otherwise returns non-zero value.
 */
 int main(void)
 {
-puts("\"Programming is like buliding a multilingual puzzle");
+puts("Programming is like buliding a multilingual puzzle");
 return (0);
 }
-
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,18 @@
-#include <stdio.h>-e
+#include <stdio.h>
 /*
- *main is the entry point for yhe program
+ *main- is the entry point for yhe program
+ *
+ *Description: 'the program's description'
  *
  *return: zero if no error, non-zero value if errors.
 */
 int main(void)
 {
-printf("Size of a char: %id byte(s)\n",sizeof(char));
-printf("Size of an int: %id byte(s)\n",sizeof(int));printf("Size of a long int: %id byte(s)\n",sizeof(long int));
-printf("Size of a long long int: %id byte(s)\n",sizeof(long long int));
-printf("Size of a float: %id byte(s)\n",sizeof(float));
-return(0)
+printf("Size of a char: %id byte(s)\n", sizeof(char));
+printf("Size of an int: %id byte(s)\n", sizeof(int));
+printf("Size of a long int: %id byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %id byte(s)\n", sizeof(long long int));
+printf("Size of a float: %id byte(s)\n", sizeof(float));
+}
+return(0);
   
